e0a03492fd8451ac60e5346e2c284d9bb4b36a43
Alexandre do Amaral Marti <alexandremarti@gmail.com>
2018-02-24 11:37:47 -0300

    Adicionado comentário sobre append do slice

--- golang/CURSO-GOLANG/arrayslicemap/slicemake/slicemake.go
+++ golang/CURSO-GOLANG/arrayslicemap/slicemake/slicemake.go
@@ -13,6 +13,9 @@ func main() {
 	s = append(s, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
 	fmt.Println(s, len(s), cap(s))
 
+	// ao adicionar um novo elemente além do tamanho, o array interno é dobrado
+	// se o meu array estiver com 20 passa para 40
+	// se o meu array estiver com 40 passa para 80
 	s = append(s, 1)
 	fmt.Println(s, len(s), cap(s))
 }
